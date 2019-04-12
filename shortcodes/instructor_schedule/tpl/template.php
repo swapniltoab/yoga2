eb1e1e7d18fb37262b6b04ebb7c6138cb8690665
vidyapawar95 <vpawar@cemtrexlabs.com>
2019-04-12 17:14:33 +0530

    done mobile calender changes

--- shortcodes/instructor_schedule/tpl/template.php
+++ shortcodes/instructor_schedule/tpl/template.php
@@ -1,4 +1,5 @@
-<div class="main-calender-div">
+<div class="desk-calender">
+    <div class="main-calender-div">
     <div class="schedule-table-week-controller text-uppercase small bg-white text-center pt-2 pb-2 font-weight-bold">
         <div class="container">
             <div class="row">
@@ -85,4 +86,120 @@
         ?>
     </div>
 </div>
+</div>
+
+
+<div class="mob-calender">
+  <div class="main-calender-div">
+    <div class="schedule-table-week-controller text-uppercase small bg-white text-center pt-2 pb-2 font-weight-bold">
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <span data-action="prev" class="prev navigator greyed">&lt;&lt;</span>
+                </div>
+                <div class="col-8">
+                    <div class="this-week-banner date-display active">
+                    This Week
+                    </div>
+                    <div class="js-current-dt"></div>
+                </div>
+                    <span data-action="next" class="next navigator">&gt;&gt;</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="schedule-table" class="schedule-table row no-gutters" data-currenttime="1550488645">
+        <div class="col-lg bg-white text-uppercase small" id="">
+                    <div class="nav nav-tabs">
+        <?php
+        $i = 0;
+        foreach ($schedule as $classes) {
+             $i++;
+            $active = $i == 1 ? 'active' : '';
+           // print_r($classes);
+             if(array_key_exists('isEmpty',$classes) && $classes['isEmpty'] == 1){ ?>
+                        <div data-toggle="tab" class="test js-day-div <?php echo $active ?>" href="#menu-<?php echo $classes[0]['date'] ?>" data-cur-dt="<?php echo $classes[0]['weekDate'] ?>" style="display:none">
+                            <div class="class-day-title p-3">
+                                <h3 class="font-weight-bold"><?php echo substr($classes[0]['day'], 0, 2) ?></h3>
+                            </div>
+                        </div>
+            <?php }
+                else {
+                    $length = count($classes);
+                    foreach ($classes as $key => $class) {
+                        
+                        if ($key == 0): ?>
+                                <div data-toggle="tab" class="test js-day-div <?php echo $active ?>" href="#menu-<?php echo $class['date'] ?>" data-cur-dt="<?php echo $classes[0]['weekDate'] ?>" style="display:none">
+                                    <div class="class-day-title p-3">
+                                        <h3 class="font-weight-bold"><?php echo substr($class['day'], 0, 2); ?></h3>
+                                    </div>
+                                </div>
+                        <?php endif;?><?php
+                    }
+             }
+
+        } ?>
+        </div>
+                </div>
+    </div>
+    <div class="tab-content">
+        <?php
+        $i = 0;
+        foreach ($schedule as $k => $classes) {
+            $i++;
+            $active = $i == 1 ? 'active' : 'fade';
+
+            if(array_key_exists('isEmpty',$classes) && $classes['isEmpty'] == 1){ ?>
+                <div id="menu-<?php echo $classes[0]['date'] ?>" class="classes-container tab-pane <?php echo $active ?>">
+                    <div class="">
+                        <span>No Data Available</span>
+                    </div>
+                </div>
+            <?php }
+
+                else {
+                $length = count($classes);
+                foreach ($classes as $key => $class) {
+                     if ($key == 0):
+                    ?>
+                     <div id="menu-<?php echo $class['date'] ?>" class="classes-container tab-pane <?php echo $active ?>">
+                     <?php
+                     endif;
+                     ?>
+                     <div class="js-container" data-instructor="<?php echo $class['instructor_name'] ?>" data-class-type="<?php echo $class['classType'] ?>">
+                        <div class="class-instructor position-relative">
+                            <div class="col-8 px-0">
+                            <div class="class-type">
+                                <?php echo $class['classType'] ?>
+                            </div>
+                            <div class="instructor-name">
+                                <?php echo $class['instructor_name'] ?><br>
+                            </div>
+                            <div class="class-time">
+                                <?php echo $class['time'] ?>
+                            </div>
+                </div>
+                         <div class="col-4 px-0">
+                            <a href="/book/?classId=<?php echo $class['class_Id'] ?>" class="reserve btn-register" type="button" data-room-id="<?php echo $class['room_Id'] ?>" data-class-id="<?php echo $class['class_Id'] ?>">RESERVE</a>
+                        </div>
+                        </div>
+                        </div>
+                    <?php  if ($key == ($length - 1)): ?>
+                            </div>
+                    <?php endif;
+                }
+             }
+
+        }
+?>
+    </div>
+
+</div>
+
+
+
+
+
 
